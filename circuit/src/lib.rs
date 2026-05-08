985c195df0c7e3f4a18ac8523f82e671c66bcab3
Voice of Miyagi <voice-of-miyagi@proton.me>
2026-05-08 16:02:31 +0200

    Implement a simple taskfile doing fetch wit and building a wasi component

--- circuit/src/lib.rs
+++ circuit/src/lib.rs
@@ -1,1 +1,19 @@
-mod phoenix;
+wit_bindgen::generate!({
+    world: "circuit-world",
+    path: "../wit-definitions/wit",
+    generate_all,
+});
+
+struct Component;
+
+impl Guest for Component {
+    fn run(_trigger_action: TriggerAction) -> Result<Vec<WasmResponse>, String> {
+        Ok(vec![WasmResponse {
+            payload: vec![],
+            ordering: None,
+            event_id_salt: None,
+        }])
+    }
+}
+
+export!(Component);
